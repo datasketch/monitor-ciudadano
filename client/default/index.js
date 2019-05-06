f69b93df03cd1aaeae4325cebaf1cdef55922e9e
marianaviro <m.villamizar564@uniandes.edu.co>
2019-05-06 11:58:38 -0500

    Test

--- client/default/index.js
+++ client/default/index.js
@@ -4,7 +4,6 @@ import './default.css';
 // import './cards.css';
 // import './footer.css';
 // import './reset.css';
-console.log('Hola default');
 
 // Hot Module Replacement [HMR]
 if (module.hot) {
