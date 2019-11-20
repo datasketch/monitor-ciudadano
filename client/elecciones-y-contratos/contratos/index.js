90964a172ff471b823355792f93cd1947cac6889
marianaviro <m.villamizar564@uniandes.edu.co>
2019-11-20 10:16:34 -0500

    Added app selector on contracts visualizer

--- client/elecciones-y-contratos/contratos/index.js
+++ client/elecciones-y-contratos/contratos/index.js
@@ -4,3 +4,31 @@ import './eyc_contratos.css';
 if (module.hot) {
 	module.hot.accept();
 }
+
+var app_campanas = document.getElementById('contratos-campanas');
+var app_partidos = document.getElementById('contratos-partidos');
+
+function showCampanas (e) {
+	e.target.style.background = '#137fc0';
+	document.getElementById('btn-partidos').style.background = '#0F669B';
+	app_campanas.classList.remove('hidden');
+	if (!app_partidos.classList.contains('hidden')) {
+		app_partidos.classList.add('hidden');
+	}
+}
+
+function showPartidos (e) {
+	e.target.style.background = '#137fc0';
+	document.getElementById('btn-campanas').style.background = '#0F669B';
+	app_partidos.classList.remove('hidden');
+	if (!app_campanas.classList.contains('hidden')) {
+		app_campanas.classList.add('hidden');
+	}
+}
+
+
+var btn_campanas = document.getElementById('btn-campanas');
+btn_campanas.addEventListener('click', showCampanas);
+
+var btn_partidos = document.getElementById('btn-partidos');
+btn_partidos.addEventListener('click', showPartidos);
